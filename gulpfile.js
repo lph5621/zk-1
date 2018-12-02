979329639fc24a1d69e2177b623bb8b931b34962
lipanhong <1207006974@qq.com>
2018-12-02 20:17:03 +0800

    模拟swiper

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var fs = require("fs");
+var url = require("url");
+var server = require("gulp-webserver");
+var path = require("path");
+var list = require("./src/mock/list.json");
 
 //编译scss
 gulp.task("devSass",function(){
@@ -12,5 +17,34 @@ gulp.task("watch",function(){
     return gulp.watch("./src/scss/index.scss",gulp.series("devSass"))
 })
 
+gulp.task("server",function(){
+    return gulp.src("./src")
+    .pipe(server({
+        port:9898,
+        open:true,
+        middleware:function(req,res,next){
+            var pathname = url.parse(req.url).pathname;
+
+            if(pathname === "/favicon.ico"){
+                res.end("");
+                return
+            }
+
+            if(pathname==="/api/list"){
+                res.end(JSON.stringify({
+                    code:1,
+                    data:list
+                }))
+            }else{
+                pathname = pathname ==="/" ? "index.html" : pathname;
+                res.end(fs.readFileSync(path.join(__dirname,"src",pathname)));
+            }
+           
+        }
+    }))
+
+    
+})
+
 //开发环境
-gulp.task("dev",gulp.series("devSass","watch"))
+gulp.task("dev",gulp.series("devSass","server","watch"))
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
-var swiper = new Swiper("#container",{
-    autoplay:true
+
+
+$.ajax({
+    url:"/api/list",
+    dataType:"json",
+    success:function(res){
+        console.log(res)
+        if(res.code == 1){
+            renderData(res.data)
+        }
+    }
 })
+function renderData(data){
+    var str = '';
+    data.forEach((i) => {
+        str += ` <div class="swiper-slide">`
+            i.list.forEach((item)=>{
+                str += `<p><img src="${item.img}" alt=""><span>${item.title}</span></p>`
+            })              
+        str +=  `</div>`
+    });
+
+    $(".swiper-wrapper").html(str);
+
+    var swiper = new Swiper("#container",{
+        autoplay:true
+    })
+}
+
